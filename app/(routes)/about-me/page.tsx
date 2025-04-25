68c4343bb9bc94fb02cd279ad3aedcfcb7f56ce2
MatiasNegrelli <matutecba19@gmail.com>
2025-04-25 12:34:53 -0300

    Remove avatar

--- app/(routes)/about-me/page.tsx
+++ app/(routes)/about-me/page.tsx
@@ -1,6 +1,5 @@
 import TransitionPage from "../../TransitionPage";
 import ContainerPage from "../../../components/ContainerPage";
-import Avatar from "@/components/Avatar";
 import BackgroundPage from "@/components/BackgroundPage";
 import CounterServices from "@/components/CounterServices";
 import TimeLine from "@/components/TimeLine";
@@ -11,7 +10,6 @@ const PageAboutMe = () => {
       <TransitionPage />
       <BackgroundPage />
       <ContainerPage>
-        <Avatar />
         <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10">
           Mi trayectectoria{" "}
           <span className=" font-bold text-green-500">en el mundo IT</span>
